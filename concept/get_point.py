88a69f06ab5fe944bd655353d698393f0eed2ce9
admchr <admchr@localhost>
2011-05-24 15:44:02 +0200

    refaktoring ekstraktora punkt?w

--- concept/get_point.py
+++ concept/get_point.py
@@ -12,22 +12,47 @@ except:
 
 files = glob.glob('img/wc_robots*.png')
 
-poss = (
-(441, 47, 'różowy najwyższy '),
-(436, 58, 'zielonkawy najwyższy '),
-(544, 347, 'żółty prawy dół'),
-(177, 364, 'żółty lewy dół'),
-(327, 116, 'środek góra żółty'),
-(184, 364, 'jasnożółty lewy dół'),
-(433, 50, 'jasnożłóty najwyższy '),
-(443, 55, 'jasnoniebieski najwyższy '),
-(508, 80, 'jasnoniebieski górny prawy'),
-(500, 82, 'niebieski górny prawy'),
-(533, 351, 'niebieski prawy dół'),
-(505, 72, 'niebieski góra prawy'),
-(317, 120, 'środek góra niebieski'),
-(184, 357, '(inny?) niebieski lewy dół'),
+col_blue = 'blue'
+col_lblue = 'lblue'
+col_yellow = 'yellow'
+col_lyellow = 'lyellow'
+col_red = 'red?'
+col_green = 'green'
+col_unknown = '?'
+
+color_mapping = {
+    col_red : (255, 0, 0),
+    col_green : (0, 255, 0),
+    col_blue : (0, 0, 255),
+    col_lblue : (128, 128, 255),
+    col_yellow : (255, 255, 0),
+    col_lyellow : (255, 255, 128)
+}
+
+
+poss_l = (
+((441, 47), col_red, 'różowy najwyższy '),
+((436, 58), col_green, 'zielonkawy najwyższy '),
+((544, 347), col_yellow,'żółty prawy dół'),
+((177, 364), col_yellow, 'żółty lewy dół'),
+((327, 116), col_yellow, 'środek góra żółty'),
+((500, 82), col_yellow, 'górny prawy'),
+((184, 364), col_lyellow, 'lewy dół'),
+((433, 50), col_lyellow, 'najwyższy'),
+((443, 55), col_lblue, 'najwyższy'),
+((508, 80), col_lblue, 'górny prawy'),
+((533, 351), col_blue, 'prawy dół'),
+((505, 72), col_blue, 'góra prawy'),
+((317, 120), col_blue, 'środek góra'),
+((184, 357), col_blue, 'lewy dół'),
 )
+samples = {
+    i: {'pos':poss_l[i][0], 'col':poss_l[i][1], 'desc':poss_l[i][2]} 
+        for i in range(len(poss_l))
+    }
+
+
+data={}
 
 imgz={}
 for fname in files:
@@ -36,39 +61,70 @@ for fname in files:
     imgz[fname] = img
     cv.Smooth(img0, img, smoothtype=cv.CV_MEDIAN, param1=3, param2=3)
 
-for (posx, posy, desc) in poss:
-    print posx, posy, desc
-    print ' r      g     b,     h     s     v'
-    for fname in files:
-        img = imgz[fname]
-        # print fname
+
+for fname in files:
+    img = imgz[fname]
+    # print fname
+    
+    imgA, imgB, imgC = [cv.CreateMat(img.rows, img.cols, cv.CV_8UC1) for i in [1,2,3]]
+    cv.CvtColor(img, img, cv.CV_RGB2HSV)
+    
+    cv.Split(img, imgA, imgB, imgC, None)
+    
+    for i, s in samples.iteritems():
+        d = data[fname,i] = {}
+        (posx, posy) = s['pos']
+        d['h']=cv.Get2D(imgA, posy, posx)[0]
+        d['s']=cv.Get2D(imgB, posy, posx)[0]
+        d['v']=cv.Get2D(imgC, posy, posx)[0]
+    
+    cv.Merge(imgA, imgB, imgC, None, img)
+    
+    cv.CvtColor(img, img, cv.CV_HSV2RGB)
+    
+    cv.Split(img, imgA, imgB, imgC, None)
+    
+    for i, s in samples.iteritems():
+        d = data[fname,i]
+        (posx, posy) = s['pos']
+        d['b']=cv.Get2D(imgA, posy, posx)[0]
+        d['g']=cv.Get2D(imgB, posy, posx)[0]
+        d['r']=cv.Get2D(imgC, posy, posx)[0]
         
-        imgA, imgB, imgC = [cv.CreateMat(img.rows, img.cols, cv.CV_8UC1) for i in [1,2,3]]
-        cv.CvtColor(img, img, cv.CV_RGB2HSV)
+        fill_name = s['col']
+        if fill_name in color_mapping:
+            fill = color_mapping[fill_name]
+        else:
+            fill = (255, 255, 255)
         
-        cv.Split(img, imgA, imgB, imgC, None)
+        cv.Set2D(imgA, posy, posx, fill[2])
+        cv.Set2D(imgB, posy, posx, fill[1])
+        cv.Set2D(imgC, posy, posx, fill[0])
         
-        h=cv.Get2D(imgA, posy, posx)[0]
-        s=cv.Get2D(imgB, posy, posx)[0]
-        v=cv.Get2D(imgC, posy, posx)[0]
-        
-        cv.Merge(imgA, imgB, imgC, None, img)
-        
-        cv.CvtColor(img, img, cv.CV_HSV2RGB)
-        
-        cv.Split(img, imgA, imgB, imgC, None)
-        
-        b=cv.Get2D(imgA, posy, posx)[0]
-        g=cv.Get2D(imgB, posy, posx)[0]
-        r=cv.Get2D(imgC, posy, posx)[0]
-        
-        cv.Set2D(imgA, posy, posx, 255)
-        cv.Set2D(imgB, posy, posx, 255)
-        cv.Set2D(imgC, posy, posx, 255)
-        
-        cv.Merge(imgA, imgB, imgC, None, img)
-        
-        print "%5d %5d %5d, %5d %5d %5d    %s"%(r, g, b, h, s, v, fname) 
+    cv.Merge(imgA, imgB, imgC, None, img)
+    
 
 for fname in files:
     cv.SaveImage('out_px/'+os.path.basename(fname), imgz[fname])
+
+
+for i, s in samples.iteritems():
+    print "%s %s %s"%(s['pos'], s['col'], s['desc'])
+    
+    minimum = dict(data[files[0],i])
+    maximum = dict(data[files[0],i])
+    for fname in files:
+        d = data[fname,i]
+        for k in d:
+            minimum[k]=min(d[k], minimum[k])
+            maximum[k]=max(d[k], maximum[k])
+    
+    for d in (minimum, maximum):
+        print "%5d %5d %5d, %5d %5d %5d"%(d['r'], d['g'], d['b'], d['h'], d['s'], d['v'])
+    print ' r      g     b,     h     s     v' 
+    for fname in files:
+        d=data[fname,i]
+        print "%5d %5d %5d, %5d %5d %5d    %s"%(d['r'], d['g'], d['b'], d['h'], d['s'], d['v'], fname[14:-3]) 
+
+"""    print posx, posy, desc
+    """
