b9bfabb03dc0ee0e285248d2201dadc251d200f1
admchr <admchr@localhost>
2011-06-12 19:05:55 +0200

    dodany balans bieli do rozpoznawaczki, runner rozpoznawarki przepisany do pythona
    (przyda si? potem)

--- src/tools.h
+++ src/tools.h
@@ -30,8 +30,28 @@ inline uchar rgb_to_srgb(uchar ch){
 inline cv::Vec3b srgb_to_rgb(cv::Vec3b val){
     return cv::Vec3b(srgb_to_rgb(val[0]), srgb_to_rgb(val[1]), srgb_to_rgb(val[2]));
 }
+
 inline cv::Vec3b rgb_to_srgb(cv::Vec3b val){
     return cv::Vec3b(rgb_to_srgb(val[0]), rgb_to_srgb(val[1]), rgb_to_srgb(val[2]));
+}
+
+inline int white_correct(cv::Mat_<cv::Vec3b>& img, cv::Point p, int radius){
+    
+    // ugly
+    cv::Mat_<cv::Vec3b> median = img.clone();
+    cv::medianBlur(median, median, radius);
+    cv::Vec3b color = median(p);
+    
+    int minval = std::min(color[0], std::min(color[1], color[2]));
+    
+    std::vector<cv::Mat> v;
+    
+    cv::split(img, v);
+    for (int i=0;i<3;i++)
+        v[i]*=200*1.0/color[i];
+    cv::merge(v, img);
+    
+    return minval;
 }
 
 #endif
