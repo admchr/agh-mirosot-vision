74d5261f73a63cad341b97ed5e8842669805a58b
admchr <admchr@localhost>
2011-08-21 19:55:01 +0200

    configuration loading, base for white balance

--- src/perf.cpp
+++ src/perf.cpp
@@ -1,5 +1,6 @@
 
 #include "header.h"
+#include "common.hpp"
 
 #include <opencv/cv.h>
 #include <vector>
@@ -16,6 +17,7 @@ int main(int argc, char**argv) {
     init_config(&config);
     config.width = img0.size().width;
     config.height = img0.size().height;
+    load_config(&config, argv[2]);
     
     for (int i=0;i<RUNS;i++) {
         cv::Mat_<cv::Vec3b> img_tmp(img0);
