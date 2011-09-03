6197878433390a5295b5669ecfe4461ba770b90b
admchr <admchr@localhost>
2011-09-04 01:57:01 +0200

    failed attempt to write my own meanshift procedure

---
+++ src/defs.hpp
@@ -0,0 +1,7 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <opencv/cv.h>
+typedef cv::Mat_<cv::Vec3b> Image;
+
+#endif
--- src/mshift.hpp
+++ src/mshift.hpp
@@ -1,10 +1,16 @@
 #ifndef MSHIFT_H 
 #define MSHIFT_H
 
+#include "defs.hpp"
+
 #include <opencv/cv.h>
+// TODO: where is std::pair?
+#include <map>
+
+std::pair<cv::Point, cv::Vec3b> meanShiftStep(cv::Point p, cv::Vec3b color, const Image& img, int width, int height);
 
 CV_IMPL void
-meanShiftFiltering( cv::Mat src0, cv::Mat dst0,
+meanShiftFiltering( Image src0, Image dst0,
     double sp0, double sr, int max_level,
     CvTermCriteria termcrit );
 #endif
